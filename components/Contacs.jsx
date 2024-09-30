7a94ea3a258edc9b3244e61c7d575b6dcb1f8092
dhirendra <shindedhirendra780@gmail.com>
2024-09-30 18:50:11 +0530

    navigation setup

---
+++ components/Contacs.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class Contacs extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Contacts </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
